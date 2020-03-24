40c50a0b3660c34228857c385416553894bb14dc
huntercfreeman <huntercfreeman@gmail.com>
2020-03-23 20:18:30 -0400

    working on the assertions

--- main.c
+++ main.c
@@ -4,13 +4,34 @@
 #include <string.h>
 #include "singlyLinkedList.h"
 
+int NULLTests();
+int OneNULLTests();
+
 int main()
 {
   // NULL Tests
+  NULLTests();
+
+  // 1->NULL Tests
+  OneNULLTests();
+
+  // 1->2->NULL Tests
+
+  // 1->2->3->NULL Tests
+  return 0;
+}
+
+int NULLTests()
+{
+  return 0;
+}
+
+int OneNULLTests()
+{
   singlyLinkedListNode* head = (singlyLinkedListNode*)malloc(sizeof(singlyLinkedListNode));
-  head->value = 10;
+  head->value = 1;
   char* asString = singlyLinkedListAPI.ToString(head);
-  assert(strcmp("10",asString) == 0);
+  assert(strcmp("1",asString) == 0);
 
   return 0;
 }
