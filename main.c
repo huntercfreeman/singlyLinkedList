02aac9fbfeadf645d088a06645a27949b2f40df7
huntercfreeman <huntercfreeman@gmail.com>
2020-03-25 10:40:58 -0400

    Finished null assertions

--- main.c
+++ main.c
@@ -77,6 +77,58 @@ int NULLTests()
   asString = singlyLinkedListAPI.ToString(temporary1);
   assert(NULL == asString);
 
+  // Clear
+  temporary1 = singlyLinkedListAPI.Clear(NULL);
+  asString = singlyLinkedListAPI.ToString(temporary1);
+  assert(NULL == asString);
+
+  // Contains
+  int contains = singlyLinkedListAPI.Contains(NULL, 5);
+  assert(0 == contains);
+
+  // Equal
+  int equal = singlyLinkedListAPI.Equal(NULL, NULL);
+  assert(1 == equal);
+
+  // Count
+  int count = singlyLinkedListAPI.Count(NULL);
+  assert(0 == count);
+
+  // FindFirst
+  temporary1 = singlyLinkedListAPI.FindFirst(NULL, 5);
+  asString = singlyLinkedListAPI.ToString(temporary1);
+  assert(NULL == asString);
+
+  // FindLast
+  temporary1 = singlyLinkedListAPI.FindLast(NULL, 5);
+  asString = singlyLinkedListAPI.ToString(temporary1);
+  assert(NULL == asString);
+
+  // RemoveNode
+  temporary1 = singlyLinkedListAPI.RemoveNode(NULL, NULL, NULL);
+  asString = singlyLinkedListAPI.ToString(temporary1);
+  assert(NULL == asString);
+
+  // RemoveFirst
+  temporary1 = singlyLinkedListAPI.RemoveFirst(NULL, 5);
+  asString = singlyLinkedListAPI.ToString(temporary1);
+  assert(NULL == asString);
+
+  // RemoveLast
+  temporary1 = singlyLinkedListAPI.RemoveLast(NULL, 5);
+  asString = singlyLinkedListAPI.ToString(temporary1);
+  assert(NULL == asString);
+
+  // RemoveStart
+  temporary1 = singlyLinkedListAPI.RemoveStart(NULL);
+  asString = singlyLinkedListAPI.ToString(temporary1);
+  assert(NULL == asString);
+
+  // RemoveEnd
+  temporary1 = singlyLinkedListAPI.RemoveEnd(NULL);
+  asString = singlyLinkedListAPI.ToString(temporary1);
+  assert(NULL == asString);
+
   return 0;
 }
 
